c60d55311bfd909f4c42b1f42307316fa1ad38ee
KhaQuy <khaquydev04@gmail.com>
2025-07-23 15:29:29 +0700

    fix file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-
 import Loading from "./components/LoadingComponent/Loading";
 import "./App.css";
-import Home from "./pages/User/Home";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Loading />
-        <Home />
       </header>
     </div>
   );
