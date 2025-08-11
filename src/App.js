b103569e193d3dda84a57be6b71c1cadaf338a9c
Tran Huu Minh Quan <minhquantranhuu2004@gmail.com>
2025-08-11 19:01:11 +0700

    fix lần 4

--- src/App.js
+++ src/App.js
@@ -6,30 +6,15 @@ import { Routing } from './Routing/Routing';
 
 function App() {
   return (
-    // <BrowserRouter>
       <div className="App">
         <Header />
         <div className="main-layout">
           <Sidebar />
           <div className="content">
             <Routing />
-            {/* <Routes>
-              <Route path="/account/userprofile" element={<div>userprofile</div>} />
-              <Route path="/duyet-phong-kham" element={<Clinicbrowses />} />
-              <Route path="/duyet-phong-kham/thong-tin-phong-kham/:id" element={<Detailclinicbrowse />} />
-              <Route path="/duyet-bac-si" element={<Doctorbrowses />} />
-              <Route path="/quan-ly-phong-kham" element={<Clinicmanagers />} />
-              <Route path="/quan-ly-goi/tao-goi" element={<div>Create Package</div>} />
-              <Route path="/quan-ly-goi/danh-sach-goi" element={<Alllistpackages />} />
-              <Route path="/thong-ke-bao-cao/tong-quan" element={<div>Overview Statistics</div>} />
-              <Route path="/thong-ke-bao-cao/thong-ke-doanh-thu" element={<div>Revenue Statistics</div>} />
-              <Route path="/doi-mat-khau" element={<div>Change Password</div>} />
-              <Route path="/account/login" element={<div>Login</div>} />
-            </Routes> */}
           </div>
         </div>
       </div>
-    // </BrowserRouter>
   );
 }
 
